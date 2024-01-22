8e470ff2b188f5e3bf95990ac5dc30620787df17
threehund0 <samuelbaik87@gmail.com>
2024-01-21 17:02:30 -0800

    GET /api/pokemon/:id path added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const pokedex = require('./base1')
 const PORT = 8000
+app.use(express.json())
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html')
@@ -10,6 +11,23 @@ app.get('/', (req, res) => {
 // GET /api/pokedex
 app.get('/api/pokedex', (req, res) => {
 	res.json(pokedex)
+})
+
+// GET /api/pokedex/:id
+app.get('/api/pokedex/:name', (req, res) => {
+	const name = req.params.name.toLowerCase()
+	console.log(pokedex.find(pokemon => pokemon.name.toLowerCase() === name))
+
+	if (pokedex.find(pokemon => pokemon.name.toLowerCase() === name)) {
+		const singleCard = pokedex.find(
+			pokemon => pokemon.name.toLowerCase() === name
+		)
+		return res
+			.status(200)
+			.json({ name: singleCard.name, img: singleCard.images.small })
+	} else {
+		return res.json(`Pokemon card not found`)
+	}
 })
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
